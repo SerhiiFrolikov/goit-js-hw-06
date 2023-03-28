0d7d9c76ff9015792632773fae962e19bf82c95d
SerhiiFrolikov <116305785+SerhiiFrolikov@users.noreply.github.com>
2023-03-28 23:56:22 +0300

    task 9 is done with question

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const bodyColor = document.querySelector('body');
+const btnChangeColor = document.querySelector('button.change-color');
+const valueChangeColor = document.querySelector('span.color');
+
+const changeColor = () => {
+  valueChangeColor.textContent = bodyColor.style.backgroundColor = getRandomHexColor();
+}
+btnChangeColor.addEventListener('click', changeColor);

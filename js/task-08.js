9ed26e4ae17f22db5c528b98e73d160161ea253a
SerhiiFrolikov <116305785+SerhiiFrolikov@users.noreply.github.com>
2023-03-28 22:50:03 +0300

    task 8 is done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const {
+        elements: { email, password }
+    } = event.currentTarget;
+
+    if (email.value === "" || password.value === "") {
+        return alert("Будь-ласка заповніть усі поля!");
+    } else {
+        const loginFormData = {
+        email: email.value,
+        password: password.value,
+    };
+
+    console.log(loginFormData);
+        event.currentTarget.reset();
+}
+}
